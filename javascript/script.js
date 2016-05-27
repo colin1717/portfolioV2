3a28b2819f5f3e3af2df363ac2c9a540e2aad022
Colin Hancock <colin1717@mac.com>
2016-05-27 15:55:50 -0500

    project1 working

--- javascript/script.js
+++ javascript/script.js
@@ -10,8 +10,11 @@ $(window).scroll(function(){
     'transform' : 'translate(0px, ' + wScroll /16 + '%)'
   });
 
-  if(wScroll > $('.project1').offset().top - ($(window).height() / 1.5)) {
+  if(wScroll > $('.project1').offset().top - ($(window).height() / 1.6) && wScroll < 1000) {
     console.log('balls');
+    $('.project1 .image').css({
+      'right' : wScroll - ($(window).height() / 2)
+    })
     $('.project1 .description').show(500);
   };
 
