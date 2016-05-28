b3c80da51bb86b01f5b918f4e848c7c252e7f75e
Colin Hancock <colin1717@mac.com>
2016-05-28 14:21:42 -0500

    all boxes moving janky

--- javascript/script.js
+++ javascript/script.js
@@ -15,18 +15,31 @@ $(window).scroll(function(){
 
   shiftLeft('#take-me-home .project-pic', '#take-me-home .project-description', 1.7, 2, wScroll);
 
+  shiftRight('#shining .project-pic', '#shining .project-description', 1.45, 2, wScroll);
+
   shiftLeft('#upcoming .project-pic', '#upcoming .project-description', 1.5, .55, wScroll);
 
 });
 
 function shiftLeft(projectPic, projectDescription, val, val2, wScroll) {
   if(wScroll > $(projectPic).offset().top - ($(window).height() / val)) {
-    console.log(projectPic);
     $(projectPic).css({
       'right' : wScroll - ($(window).height() / val2)
     });
     $(projectDescription).show(500);
   } else if (wScroll < $(projectPic).offset().top - ($(window).height() / val)) {
-    $(projectDescription).hide(500);
+      $(projectDescription).hide(500);
+  }
+}
+
+function shiftRight(projectPic, projectDescription, val, val2, wScroll) {
+  if(wScroll > $(projectPic).offset().top - ($(window).height() / val)) {
+    console.log(projectPic);
+    $(projectPic).css({
+      'left' : wScroll - ($(window).height() / val2)
+    });
+    $(projectDescription).show(500);
+  } else if (wScroll < $(projectPic).offset().top - ($(window).height() / val)) {
+      $(projectDescription).hide(500);
   }
 }
