5a77d8686c8df03308a62158e34e38b017920524
Colin Hancock <colin1717@mac.com>
2016-05-28 14:03:50 -0500

    1st and 3rd scrolling janky

--- javascript/script.js
+++ javascript/script.js
@@ -3,6 +3,7 @@ $(window).scroll(function(){
   var wScroll = $(this).scrollTop();
 
   console.log(wScroll);
+  console.log('This is propic top: ' + ($('#upcoming .project-pic').offset().top - ($(window).height()) / 1.5));
 
   $('.mac').css({
     'transform' : 'translate(0px, ' + wScroll /8 + '%)'
@@ -12,15 +13,20 @@ $(window).scroll(function(){
     'transform' : 'translate(0px, ' + wScroll /16 + '%)'
   });
 
-  if(wScroll > $('.project1').offset().top - ($(window).height() / 1.6) && wScroll < 1000) {
-    $('.project1 .image').css({
-      'right' : wScroll - ($(window).height() / 2)
-    });
-    $('.project1 .description').show(500);
-  };
+  shiftLeft('#take-me-home .project-pic', '#take-me-home .project-description', 1.7, 2, wScroll);
 
-  if(wScroll < ($('.project1').offset().top / 1.6)) {
-    $('.project1 .description').hide(500);
-  }
+  shiftLeft('#upcoming .project-pic', '#upcoming .project-description', 1.5, .55, wScroll);
 
 });
+
+function shiftLeft(projectPic, projectDescription, val, val2, wScroll) {
+  if(wScroll > $(projectPic).offset().top - ($(window).height() / val)) {
+    console.log(projectPic);
+    $(projectPic).css({
+      'right' : wScroll - ($(window).height() / val2)
+    });
+    $(projectDescription).show(500);
+  } else if (wScroll < $(projectPic).offset().top - ($(window).height() / val)) {
+    $(projectDescription).hide(500);
+  }
+}
